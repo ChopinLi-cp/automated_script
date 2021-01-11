ef9fac3010d2d222967cf8993ab6063a7aafe672
chengpengli <jingzhaolicheng@163.com>
2021-01-11 19:36:48 +0800

    change the structure of the files

--- run_iFixFlakies.sh
+++ run_iFixFlakies.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-SCRIPT_USERNAME="lichengpeng/test/automated_script"
+SCRIPTDIR="$(pwd)"
 TOOL_REPO="iFixFlakies"
 
 git rev-parse HEAD
 
 date
 
-cd /home/$SCRIPT_USERNAME
+cd $SCRIPTDIR
+RESULTSDIR=$SCRIPTDIR/iFixFlakies_output/
+mkdir -p ${RESULTSDIR}
 
 # This script is run for single experiment (one project)
-# Should only be invoked by the run_experiment.sh script
+# Should only be invoked by the run_iFixFlakies.sh script
 
 if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]] || [[ $5 == "" ]]; then
     echo "arg1 - GitHub SLUG"
@@ -27,14 +29,6 @@ module=$3
 rounds=$4
 timeout=$5
 
-echo $slug
-echo $sha
-echo $module
-echo $rounds
-echo $timeout
-
-RESULTSDIR=/home/$SCRIPT_USERNAME/iFixFlakies_output/
-mkdir -p ${RESULTSDIR}
 
 modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
 short_sha=${sha:0:7}
@@ -44,61 +38,44 @@ modifiedslug_with_sha="${modifiedslug}-${short_sha}"
 MVNOPTIONS="-Ddependency-check.skip=true -Dgpg.skip=true -DfailIfNoTests=false -Dskip.installnodenpm -Dskip.npm -Dskip.yarn -Dlicense.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip -Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dmdep.analyze.skip -Dpgpverify.skip -Dxml.skip -Dcobertura.skip=true -Dfindbugs.skip=true"
 IDF_OPTIONS="-Ddt.detector.original_order.all_must_pass=false -Ddetector.timeout=${timeout} -Ddt.randomize.rounds=${rounds} -fn -B -e -Ddt.cache.absolute.path=/home/lichengpeng/test/all-output/${modifiedslug}_output"
 
-# iDFlakiesVersion=1.2.0-SNAPSHOT
-
-# Setup prolog stuff
-# cd /home/$SCRIPT_USERNAME & ./setup
-
-echo $(pwd)
 # Clone the testing project
-bash /home/$SCRIPT_USERNAME/clone-project.sh "$slug" "$sha"
-cd /home/$SCRIPT_USERNAME/$slug
+bash $SCRIPTDIR/clone-project.sh "$slug" "$sha"
+cd $SCRIPTDIR/$slug
 
 if [[ -z $module ]]; then
     echo "================ Missing module. Exiting now!"
     exit 1
 else
-    echo "Module passed in from csv."
+    echo "Module passed from the flags."
 fi
 echo "Location of module: $module"
 
 # echo "================Installing the project"
-bash /home/$SCRIPT_USERNAME/install-project.sh "$slug" "$MVNOPTIONS" "$USER" "$module" "$sha" "$dir" "$fullTestName" "${RESULTSDIR}"
+bash $SCRIPTDIR/install-project.sh "$slug" "$MVNOPTIONS" "$USER" "$module" "$sha" "$dir" "$fullTestName" "${RESULTSDIR}"
 ret=${PIPESTATUS[0]}
+
 mv mvn-install.log ${RESULTSDIR}/${modifiedslug_with_sha}-mvn-install.log
+
 if [[ $ret != 0 ]]; then
     # mvn install does not compile - return 0
     echo "Compilation failed. Actual: $ret"
     exit 1
 fi
 
-# echo "================Running maven test"
-if [[ "$slug" == "dropwizard/dropwizard" ]]; then
-    # dropwizard module complains about missing dependency if one uses -pl for some modules. e.g., ./dropwizard-logging
-    MVNOPTIONS="${MVNOPTIONS} -am"
-elif [[ "$slug" == "fhoeben/hsac-fitnesse-fixtures" ]]; then
-    MVNOPTIONS="${MVNOPTIONS} -DskipITs"
-fi
-
 # Incorporate tooling into the project, using Java XML parsing
-cd "/home/$SCRIPT_USERNAME/${slug}"
-sh /home/$SCRIPT_USERNAME/$TOOL_REPO/modify-project.sh .
+cd $SCRIPTDIR/${slug}
+sh $SCRIPTDIR/$TOOL_REPO/modify-project.sh .
 
 # Run the plugin, get module test times
 echo "*******************iFIXFLAKIES************************"
 echo "Running testplugin for getting module test time"
 date
 
-modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
-
 # Optional timeout... In practice our tools really shouldn't need 1hr to parse a project's surefire reports.
 timeout ${timeout}s mvn testrunner:testplugin |& tee ${modifiedslug_with_sha}-module_test_time.log
 
-# Gather the results, put them up top
-# /home/$SCRIPT_USERNAME/gather-results $(pwd) ${RESULTSDIR}
 mv *.log ${RESULTSDIR}/
 
-echo "*******************iDFLAKIES************************"
+echo "*******************iFixFLAKIES************************"
 echo "Finished run_iFixFlakies.sh"
 date
-
--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
+SCRIPTDIR="$(pwd)"
+
 slug=$1
 sha=$2
 module=$3
 rounds=$4
 timeout=$5
 
+cd $SCRIPTDIR
 sh run_iDFlakies.sh $slug $sha $module $rounds $timeout
+
+cd $SCRIPTDIR
 sh run_iFixFlakies.sh $slug $sha $module $rounds $timeout
