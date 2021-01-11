0076f0524667caa0c26fddb0105b9012cb582601
chengpengli <jingzhaolicheng@163.com>
2021-01-11 20:00:52 +0800

    change parameters

--- script.sh
+++ script.sh
@@ -3,11 +3,10 @@ SCRIPTDIR="$(pwd)"
 slug=$1
 sha=$2
 module=$3
-rounds=$4
-timeout=$5
+timeout=$4
 
 cd $SCRIPTDIR
-sh run_iDFlakies.sh $slug $sha $module $rounds $timeout
+sh run_iDFlakies.sh $slug $sha $module $timeout
 
 cd $SCRIPTDIR
-sh run_iFixFlakies.sh $slug $sha $module $rounds $timeout
+sh run_iFixFlakies.sh $slug $sha $module $timeout
