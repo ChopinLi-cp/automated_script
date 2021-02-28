9409bbf7c5655a31dba0fbf7d9fee88ebc0d65f5
Chopin Li <31055712+ChopinLi-cp@users.noreply.github.com>
2021-03-01 02:02:22 +0800

    Update install-project.sh

--- install-project.sh
+++ install-project.sh
@@ -1,9 +1,9 @@
 slug=$1
-# MVNOPTIONS=$2
+MVNOPTIONS=$2
 module=$3
 
 echo "================Installing the project"
-mvn clean install -am -pl $module -DskipTests |& tee mvn-install.log
+mvn clean install -am -pl $module $MVNOPTIONS -DskipTests |& tee mvn-install.log
 
 ret=${PIPESTATUS[0]}
 exit $ret
